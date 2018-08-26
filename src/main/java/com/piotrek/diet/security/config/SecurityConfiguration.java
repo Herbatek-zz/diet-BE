61bb0579e81b806654c827bfd7158478a59897fd
kenshin985 <kenshin985@gmail.com>
2018-08-26 19:30:16 +0200

    a little refactor

--- src/main/java/com/piotrek/diet/security/config/SecurityConfiguration.java
+++ src/main/java/com/piotrek/diet/security/config/SecurityConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,20 +16,17 @@ import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.Filter;
 
-import static com.piotrek.diet.security.helpers.SecurityConstants.*;
+import static com.piotrek.diet.security.helpers.SecurityConstants.SIGN_IN_URL;
 
 
-@Order(200)
 @Configuration
 @EnableOAuth2Client
-@EnableAuthorizationServer
 @RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
@@ -42,7 +38,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/", "/login/**").permitAll()
-                .anyRequest().authenticated() // need to be enabled in final version
+                .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
                 .and()
