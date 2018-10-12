788f55fa1a5d60be7d42a9c1b8d5d79f128f2085
kenshin985 <kenshin985@gmail.com>
2018-10-12 22:36:43 +0200

    cartDtoConverter tests

--- src/test/java/com/piotrek/diet/cart/CartDtoConverterTest.java
+++ src/test/java/com/piotrek/diet/cart/CartDtoConverterTest.java
@@ -2,6 +2,8 @@ package com.piotrek.diet.cart;
 
 import com.piotrek.diet.meal.MealDtoConverter;
 import com.piotrek.diet.product.ProductDtoConverter;
+import com.piotrek.diet.sample.CartSample;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,13 +14,36 @@ class CartDtoConverterTest {
     private MealDtoConverter mealDtoConverter = new MealDtoConverter(productDtoConverter);
     private CartDtoConverter cartDtoConverter = new CartDtoConverter(mealDtoConverter);
 
+    private Cart cart;
+    private CartDto cartDto;
 
+    @BeforeEach
+    void setUp() {
+        cart = CartSample.cart1();
+        cartDto = CartSample.cartDto1();
+    }
 
     @Test
     void toDto() {
+        final var converted = cartDtoConverter.toDto(cart);
+
+        assertAll(
+                () -> assertEquals(cartDto.getId(), converted.getId()),
+                () -> assertEquals(cartDto.getDate(), converted.getDate()),
+                () -> assertEquals(cartDto.getUserId(), converted.getUserId()),
+                () -> assertEquals(cartDto.getMeals(), converted.getMeals())
+        );
     }
 
     @Test
     void fromDto() {
+        final var converted = cartDtoConverter.fromDto(cartDto);
+
+        assertAll(
+                () -> assertEquals(cart.getId(), converted.getId()),
+                () -> assertEquals(cart.getDate(), converted.getDate()),
+                () -> assertEquals(cart.getUserId(), converted.getUserId()),
+                () -> assertEquals(cart.getMeals(), converted.getMeals())
+        );
     }
 }
