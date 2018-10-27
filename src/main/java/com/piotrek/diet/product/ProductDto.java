3f908f79a831b36530faf66d5d1dca662d481898
kenshin985 <kenshin985@gmail.com>
2018-10-27 16:44:05 +0200

    product validation

--- src/main/java/com/piotrek/diet/product/ProductDto.java
+++ src/main/java/com/piotrek/diet/product/ProductDto.java
@@ -3,6 +3,7 @@ package com.piotrek.diet.product;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -13,36 +14,44 @@ public class ProductDto {
     private String id;
 
     @NotNull
-    @Size(min = 2, max = 80)
+    @Size(min = 2, max = 60)
     private String name;
 
     @NotNull
+    @Size(min = 10, max = 1000)
     private String description;
 
     @NotNull
     private String imageUrl;
 
     @NotNull
+    @Min(0)
     private double protein;
 
     @NotNull
+    @Min(0)
     private double carbohydrate;
 
     @NotNull
     private double fat;
 
     @NotNull
+    @Min(0)
     private double fibre;
 
     @NotNull
+    @Min(0)
     private double kcal;
 
+    @Min(0)
     private int amount;
 
     @NotNull
+    @Min(0)
     private double carbohydrateExchange;    // <-- 1.0 == 10g carbohydrate
 
     @NotNull
+    @Min(0)
     private double proteinAndFatEquivalent; // <-- 1.0 == 100kcal from fat and protein
 
     private String userId;
