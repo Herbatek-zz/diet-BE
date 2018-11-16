9aa2c5395b5d318de1b343449f5473f0e4bfdf49
kenshin985 <kenshin985@gmail.com>
2018-11-16 01:50:39 +0100

    badRequestException deleted - it wasn't used

--- src/main/java/com/piotrek/diet/helpers/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/piotrek/diet/helpers/exceptions/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
@@ -14,11 +13,5 @@ public class GlobalExceptionHandler {
     @ResponseStatus(NOT_FOUND)
     public ErrorResponse notFoundHandler(NotFoundException notFoundException) {
         return new ErrorResponse(404, notFoundException.getMessage());
-    }
-
-    @ExceptionHandler(BadRequestException.class)
-    @ResponseStatus(BAD_REQUEST)
-    public ErrorResponse badRequestHandler(BadRequestException badRequestException) {
-        return new ErrorResponse(400, badRequestException.getMessage());
     }
 }
